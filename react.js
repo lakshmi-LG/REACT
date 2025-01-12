35d203fb07ac983928c1017cde00650b7dfc643b
lakshmi-LG <lakshmigurumurthy333@gmail.com>
2025-01-12 08:53:47 +0530

    colors are asdded using style sheet

--- react.js
+++ react.js
@@ -10,7 +10,7 @@ const parent=
     React.createElement("div",{id:"child2"}, 
     [
     React.createElement("h1",{id:"h1"},"Hello from H1 child 2 tag !!"),
-    React.createElement("h4",{qp:"h4"},"hello from h4 from child 2")
+    React.createElement("h4",{id:"h4"},"hello from h4 from child 2")
     ]),
     
 ]);
