b4b49f4087c6c53139ac5e31248f9944be58729d
lakshmi-LG <lakshmigurumurthy333@gmail.com>
2025-01-20 13:36:03 +0530

    EP-2 has has added successfully

---
+++ EP-2/script.js
@@ -0,0 +1,23 @@
+var root=document.getElementById('root');
+var head=document.createElement('h1');
+head.innerHTML="Hello from JS world";
+root.appendChild(head);
+document.getElementById
+
+
+
+
+
+
+
+
+
+
+
+// replacing child in JS
+
+// var a=document.createElement("h1");
+// var root=document.getElementById('root');
+// var h1=document.getElementById('h1');
+// a.innerHTML="hello from JS";
+// root.replaceChild(a,h1);
