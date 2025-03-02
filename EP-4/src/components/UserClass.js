4b9f635a07216b10c37b4cac966d69c0a915672a
lakshmi-LG <lakshmigurumurthy333@gmail.com>
2025-03-03 00:36:55 +0530

    added 8th episode of API fetching from live data in class compo

--- EP-4/src/components/UserClass.js
+++ EP-4/src/components/UserClass.js
@@ -5,32 +5,44 @@ class UserClass extends React.Component{
         super(props);
         console.log(this.props.name + "Child construtor");
         this.state={
-            count:0,
-            count2:1  //can create multiple state_var inside this.state
+           userInfo:{
+            name:"Dummy",
+            loc:"Dummy loc"
+           }
+            
         }
     }
 
-    componentDidMount(){
+   async componentDidMount(){
         console.log(this.props.name + "Child Did Mount");
+        const data=await fetch("https://api.github.com/users/lakshmi-LG");
+        const json=await data.json();
+        console.log(json);
+
+        this.setState({
+            userInfo:json
+        })
+    }
+
+    componentDidUpdate(){
+        console.log("Did Update!! ")
+    }
+
+    componentWillUnmount(){
+        console.log("Will unmount");
     }
 
     render(){
-        const {name,loc}=this.props;
-        const {count,count2}=this.state;
+        const {login,created_at,avatar_url}=this.state.userInfo;
         console.log(this.props.name + "Child Render");
         return(
             
             <div className="User">
-                <h1>Count : {count}</h1>
-                <h1>Count2 : {count2}</h1>
-                <button onClick={()=>{
-                    this.setState({
-                        count:this.state.count + 1,
-                        // count2:this.state.count2 + 1  //can modify multiple state_var inside 1 fun (this.setState)
-                    })
-                }}>Class count +</button>
-                <h2>{name}</h2>
-                <h3>{loc}</h3>
+                
+                
+                <h2>name: {login}</h2>
+                <h3>loc: {avatar_url}</h3>
+                <h3>creation: {created_at}</h3>
             </div>
         )
     }
